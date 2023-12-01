f1b1dd1dad4f2337191829d61848c98218c73271
EC2 Default User <ec2-user@ip-172-16-31-246.ec2.internal>
2023-12-01 23:30:25 +0000

    fix

--- warmupfsx.py
+++ warmupfsx.py
@@ -8,12 +8,17 @@ import os
 def run_command(file):
     cmd = f"sudo lfs hsm_restore {file}"
     result = subprocess.run(cmd, shell=True)
+
     return True
 
 def main():
-    files = Path('/fsx/data/train').glob('**/*.jpg')
-    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+    dirs = sorted(list(Path('/fsx/data/train').iterdir()))
+    files = []
+    for i in tqdm(range(1201, 4000)):
+        files.extend(list(Path(dirs[i]).glob('*.jpg')))
+    print(len(files))
+    with concurrent.futures.ProcessPoolExecutor(max_workers=int(os.cpu_count()*4)) as executor:
         list(tqdm(executor.map(run_command, files), total=len(files), desc="Processing"))
-        
+
 if __name__ == "__main__":
     main()
